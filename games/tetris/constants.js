f80438434f3755a123f715bf147b1e14cc666bc6
EP <coralkoi44@gmail.com>
2026-06-20 07:37:39 -0400

    Split Tetris constants into module

---
+++ games/tetris/constants.js
@@ -0,0 +1,16 @@
+export const COLS = 10;
+export const ROWS = 20;
+export const NEXT_SIZE = 6;
+export const BORDER_WIDTH = 3;
+export const FILL_OVERLAP = 0.03;
+export const BASE_DROP_INTERVAL = 700;
+export const MIN_DROP_INTERVAL = 90;
+export const LEVEL_SPEED_STEP = 55;
+
+export const PIECES = ["T", "J", "L", "O", "S", "Z", "I"];
+export const COLORS = { T: 1, O: 2, L: 3, J: 4, I: 5, S: 6, Z: 7 };
+
+export const PAUSE_LABEL = "Pause";
+export const RESUME_LABEL = "Resume";
+export const FULLSCREEN_LABEL = "Fullscreen";
+export const EXIT_FULLSCREEN_LABEL = "Exit Fullscreen";
