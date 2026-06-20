e2fb04cf36f51bbcba0efdaf3cec78328227b2d9
EP <coralkoi44@gmail.com>
2026-06-20 09:06:43 -0400

    Raise Tetris scoring values

--- games/tetris/scoring.js
+++ games/tetris/scoring.js
@@ -15,13 +15,13 @@ export function scoreLineClear(player, lineCount, spin, perfectClear, speedIncre
   let difficult = false;
 
   if (spin === "tspin") {
-    base = [400, 800, 1200, 1600][lineCount] || 0;
+    base = [4000, 8000, 12000, 16000][lineCount] || 0;
     difficult = lineCount > 0;
   } else if (spin === "mini") {
-    base = lineCount === 0 ? 100 : lineCount === 1 ? 200 : 400;
+    base = lineCount === 0 ? 1000 : lineCount === 1 ? 2000 : 4000;
     difficult = lineCount > 0;
   } else {
-    base = [0, 100, 300, 500, 800][lineCount] || 0;
+    base = [0, 1000, 3000, 5000, 8000][lineCount] || 0;
     difficult = lineCount === 4;
   }
 
@@ -31,7 +31,7 @@ export function scoreLineClear(player, lineCount, spin, perfectClear, speedIncre
     player.combo += 1;
     player.lines += lineCount;
     player.score += base * level;
-    if (player.combo > 0) player.score += 50 * player.combo * level;
+    if (player.combo > 0) player.score += 500 * player.combo * level;
   } else {
     player.combo = -1;
     player.score += base * level;
@@ -41,7 +41,7 @@ export function scoreLineClear(player, lineCount, spin, perfectClear, speedIncre
   else if (lineCount > 0) player.backToBack = false;
 
   if (lineCount > 0 && perfectClear) {
-    const perfectClearBonus = [0, 800, 1200, 1800, 2000][lineCount] || 800;
+    const perfectClearBonus = [0, 8000, 12000, 18000, 20000][lineCount] || 8000;
     player.score += perfectClearBonus * level;
   }
 
